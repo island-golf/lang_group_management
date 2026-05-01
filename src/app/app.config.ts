eb8622fc4bedd2c0dee12b3b5eab97cfe80a245f
ADMIN-GOLF <vjirananon@hotmail.com>
2026-05-01 17:10:00 +0700

    fix warning

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,10 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection
 } from '@angular/core';
-import {provideRouter, RouterLink, RouterModule} from '@angular/router';
+import {provideRouter} from '@angular/router';
 
 import { routes } from './app.routes';
 import {FormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
 
 export const appConfig: ApplicationConfig = {
   providers: [
